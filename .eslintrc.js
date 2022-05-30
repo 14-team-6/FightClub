660d62ffa1a165c6b4228708e59dbd98921c32ee
Stanislav Ratashnyuk <hmstr@yandex-team.ru>
2022-05-31 02:53:42 +0300

    add webpack and linters / add missing files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+    ],
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+    },
+
+    rules: {
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                "js": "never",
+                "jsx": "never",
+                "ts": "never",
+                "tsx": "never"
+            }
+        ],
+        "no-underscore-dangle": 0,
+        "import/prefer-default-export": 0,
+        "no-restricted-syntax": [ "error", "ForInStatement", "LabeledStatement", "WithStatement" ],
+        "arrow-body-style": ["error", "always"],
+        "class-methods-use-this": 0,
+    },
+}
