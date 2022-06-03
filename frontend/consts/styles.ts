513a788aa4526feda40d8c136e3863ef23a6bae6
Danilov <andrei.danilov@netcracker.com>
2022-06-03 11:17:14 +0400

    5. delete extra const

--- frontend/consts/styles.ts
+++ frontend/consts/styles.ts
@@ -7,6 +7,3 @@ export const INPUT_BORDER_BLUE: string = '#5061FF';
 // Font sizes
 export const MAIN_TITLE_SIZE: string = '100px';
 export const INPUTS_FONT_SIZE: string = '30px';
-
-// Font
-export const PIXEBOY: string = 'Pixeboy';
