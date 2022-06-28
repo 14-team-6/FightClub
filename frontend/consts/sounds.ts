c54529d43afc65f29550823b10c35fcc3bfba484
Stanislav Ratashnyuk <hmstr@yandex-team.ru>
2022-06-28 12:55:36 +0300

    change souns to mp3

--- frontend/consts/sounds.ts
+++ frontend/consts/sounds.ts
@@ -1,7 +1,7 @@
 export const SOUND_MAIN_THEME = '/public/sounds/fight.mp3';
 export const SOUND_JUMP = '/public/sounds/jump.mp3'
-export const SOUND_ATTACK = '/public/sounds/sword.wav'
-export const SOUND_MEOW = '/public/sounds/meow.wav'
-export const SOUND_DEAD = '/public/sounds/dead.wav'
-export const SOUND_ENEMY_SWORD = '/public/sounds/attackEnemy.wav'
+export const SOUND_ATTACK = '/public/sounds/sword.mp3'
+export const SOUND_MEOW = '/public/sounds/meow.mp3'
+export const SOUND_DEAD = '/public/sounds/dead.mp3'
+export const SOUND_ENEMY_SWORD = '/public/sounds/attackEnemy.mp3'
 export const SAMPLING_RATE = 44100;
