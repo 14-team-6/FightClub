f742eec677754c494132d738343ead376b371f0d
s-ratashnyuk <101244942+s-ratashnyuk@users.noreply.github.com>
2022-06-28 18:26:39 +0100

    add assets and sounds (#32)
    * add assets and sounds
    
    * change souns to mp3
    
    Co-authored-by: Stanislav Ratashnyuk <hmstr@yandex-team.ru>

--- frontend/consts/sounds.ts
+++ frontend/consts/sounds.ts
@@ -1,3 +1,7 @@
 export const SOUND_MAIN_THEME = '/public/sounds/fight.mp3';
-export const SOUND_JUMP = '/public/sounds/jump.mp3';
+export const SOUND_JUMP = '/public/sounds/jump.mp3'
+export const SOUND_ATTACK = '/public/sounds/sword.mp3'
+export const SOUND_MEOW = '/public/sounds/meow.mp3'
+export const SOUND_DEAD = '/public/sounds/dead.mp3'
+export const SOUND_ENEMY_SWORD = '/public/sounds/attackEnemy.mp3'
 export const SAMPLING_RATE = 44100;
