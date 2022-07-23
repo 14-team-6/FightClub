ddfebbd1e3bfd29d324c7912564b715c9fb95a78
Stanislav Ratashnyuk <hmstr@yandex-team.ru>
2022-07-24 02:51:15 +0300

    small fixes

--- backend/src/services/themes.ts
+++ backend/src/services/themes.ts
@@ -3,6 +3,8 @@ import { sequelizeGlobal } from '@backend/src/components/sequelizeGlobal';
 import { ThemeUsers } from '@backend/src/models/themes/themeUsers';
 import { User } from '@backend/src/models/users/users';
 import { BaseService } from '@backend/src/services/baseService';
+import { getUserTheme } from '@backend/src/sql/getUserTheme';
+import { linkThemeToUser } from '@backend/src/sql/linkThemeToUser';
 
 type ThemeProps = {
   name: string,
@@ -34,27 +36,17 @@ export class ThemesService implements BaseService<Theme> {
   }
 
   public getUserThemes(userLogin: string): Promise<Theme[]> {
-    return sequelizeGlobal.sequelize.query(`
-      SELECT DISTINCT
-        th.id, th.name, th.data
-      FROM
-      "Themes" as th
-      INNER JOIN "ThemeUsers" as tmu ON tmu.theme_id = th.id
-      INNER JOIN "Users" as us ON us.id = tmu.user_id
-      WHERE us.login = '${userLogin}'
-      AND th."isActive" = true;
-    `, {
+    return sequelizeGlobal.sequelize.query(getUserTheme, {
       model: Theme,
       mapToModel: true,
+      replacements: { login: userLogin },
     });
   }
 
   public linkToUser(userLogin: string, themeId: number): Promise<any> {
-    return sequelizeGlobal.sequelize.query(`
-      DELETE FROM "ThemeUsers" as tmu
-      USING "Users" as us
-      WHERE tmu.user_id = us.id and us.login = '${userLogin}'
-    `).then(() => User.findOne({ where: { login: userLogin } }))
+    return sequelizeGlobal.sequelize.query(linkThemeToUser, {
+      replacements: { login: userLogin },
+    }).then(() => User.findOne({ where: { login: userLogin } }))
       .then((user: User) => ThemeUsers.create({
         userId: user.id,
         themeId,
