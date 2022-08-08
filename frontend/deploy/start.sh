2aa6d5268a9c51a62db62c903b2bedfa905b8c4b
Stanislav Ratashnyuk <hmstr@yandex-team.ru>
2022-08-08 12:17:53 +0300

    fix deploy

--- frontend/deploy/start.sh
+++ frontend/deploy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed -i -e 's@${PORT}@'"$PORT"'@' /etc/nginx/http.d/default.conf
-node /opt/app/web/serverLauncher.js &
+pm2 start /opt/app/web/serverLauncher.js &
 nginx -g 'daemon off;'
