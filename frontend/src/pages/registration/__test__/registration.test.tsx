61cae8a7b3b1560b338a8459a481ac6c85930364
Danilov <andrei.danilov@netcracker.com>
2022-08-15 22:38:20 +0300

    add tests for login and registration forms

--- frontend/src/pages/registration/__test__/registration.test.tsx
+++ frontend/src/pages/registration/__test__/registration.test.tsx
@@ -2,21 +2,23 @@ import { screen } from '@testing-library/dom';
 import { act, fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
-import LoginPageForm from '@frontend/src/components/loginPageForm/loginPageForm';
 import { BrowserRouter } from 'react-router-dom';
 import AuthService from '@frontend/src/services/authService';
+import RegisterPageForm from '@frontend/src/components/registerPageForm/registerPageForm';
 
 jest.mock('@frontend/src/services/authService');
 
-describe('Login page', () => {
+describe('Register page', () => {
   it('should not call AuthService if form is empty', async () => {
-    render(<LoginPageForm/>, { wrapper: BrowserRouter });
+    render(<RegisterPageForm/>, { wrapper: BrowserRouter });
 
     await act(async () => {
-      const el = await screen.findByText('Login');
+      const el = await screen.findByText('Register');
       fireEvent.click(el);
     });
 
-    expect(AuthService).not.toHaveBeenCalled();
+    expect(AuthService)
+      .not
+      .toHaveBeenCalled();
   });
 });
